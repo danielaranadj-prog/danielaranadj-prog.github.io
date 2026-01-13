e98d3104e73d16d29639ebc690c9644643dc9a5e
danielaranadj-prog <danielaranadj@gmail.com>
2026-01-13 07:09:40 -0700

    volvemos a normal

--- editor/js/app.js
+++
@@ -1,8 +0,0 @@
-import { loadState } from "./storage.js";
-import { renderEditor, renderPreview } from "./render.js";
-import { initEditor } from "./editor.js";
-
-loadState();
-renderEditor();
-renderPreview();
-initEditor();
